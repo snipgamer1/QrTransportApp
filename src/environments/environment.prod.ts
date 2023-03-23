2cad3d7a57d2571e1c2dce274e63bc3e8829ff63
snipgamer1 <chuchis619@hotmail.com>
2023-03-22 21:52:14 -0600

    eviroment3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    
     apiKey: "AIzaSyDbqgZ8kcJZi001bspZj2rvyOD4qpNrVCk",
     authDomain: "login-82ca6.firebaseapp.com",
+    databaseURL: "https://login-82ca6-default-rtdb.firebaseio.com",
     projectId: "login-82ca6",
     storageBucket: "login-82ca6.appspot.com",
     messagingSenderId: "977374982667",
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  production: false,
   firebaseConfig: {
-      
-      apiKey: "AIzaSyDbqgZ8kcJZi001bspZj2rvyOD4qpNrVCk",
-      authDomain: "login-82ca6.firebaseapp.com",
-      projectId: "login-82ca6",
-      storageBucket: "login-82ca6.appspot.com",
-      messagingSenderId: "977374982667",
-      appId: "1:977374982667:web:bef2134560e06c23214d0e",
-      production: true,
+    apiKey: "AIzaSyDbqgZ8kcJZi001bspZj2rvyOD4qpNrVCk",
+    authDomain: "login-82ca6.firebaseapp.com",
+    databaseURL: "https://login-82ca6-default-rtdb.firebaseio.com",
+    projectId: "login-82ca6",
+    storageBucket: "login-82ca6.appspot.com",
+    messagingSenderId: "977374982667",
+    appId: "1:977374982667:web:bef2134560e06c23214d0e"
   }
 };
 
