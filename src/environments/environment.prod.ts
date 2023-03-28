f436bc6ec088a4806586bffc784db0bdb82f471f
snipgamer1 <chuchis619@hotmail.com>
2023-03-28 02:41:34 -0600

    envio

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,19 +1,6 @@
 export const environment = {
-<<<<<<< HEAD
   production: true,
   
-//   firebaseConfig: {
-//     apiKey: "AIzaSyDbqgZ8kcJZi001bspZj2rvyOD4qpNrVCk",
-//     authDomain: "login-82ca6.firebaseapp.com",
-//     databaseURL: "https://login-82ca6-default-rtdb.firebaseio.com",
-//     projectId: "login-82ca6",
-//     storageBucket: "login-82ca6.appspot.com",
-//     messagingSenderId: "977374982667",
-//     appId: "1:977374982667:web:bef2134560e06c23214d0e"
-// }
-=======
-    production: true,
-
   firebaseConfig: {
     apiKey: "AIzaSyDbqgZ8kcJZi001bspZj2rvyOD4qpNrVCk",
     authDomain: "login-82ca6.firebaseapp.com",
@@ -23,6 +10,5 @@ export const environment = {
     messagingSenderId: "977374982667",
     appId: "1:977374982667:web:bef2134560e06c23214d0e"
 }
->>>>>>> b856bd60f0fe8a54c5e786b45f503e726574abf6
 };
 
